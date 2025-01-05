6b272a204752d1058ddc4313713c9a75fd65acf6
hlista <hlista91@gmail.com>
2025-01-05 14:41:25 -0600

    example job running

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { ApolloServer } from "apollo-server";
-import { QueueUtils } from './queue';
+import { QueueUtils, MailbotQueue, JobsEnum } from './queue';
 
 import { schema } from "./schema";
 export const server = new ApolloServer({
@@ -10,4 +10,5 @@ const port = 3000;
 server.listen({port}).then(({ url }) => {
     QueueUtils.initializeJobs();
     console.log(`🚀  Server ready at ${url}`);
+    MailbotQueue.add(JobsEnum.SEND_EMAIL, { email: 'test@test.com '});
 });
